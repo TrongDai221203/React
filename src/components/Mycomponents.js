7db0f4b216c1914b7b72095c56cdd7535906c46b
Trong Dai <buitrongdai221203@gmail.com>
2023-05-12 17:33:10 +0700

    React-VD16

---
+++ src/components/Mycomponents.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            `
+            <div>dsad</div>
+            <div>My first MyComponent</div>
+            `
+        );
+    }
+}
+
+
+export default MyComponent;
