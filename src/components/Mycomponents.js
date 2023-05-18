51a1b7933c8a71d46be8834cf6fa7c68424ffb39
Trong Dai <buitrongdai221203@gmail.com>
2023-05-18 16:47:07 +0700

    vd 19

--- src/components/Mycomponents.js
+++ src/components/Mycomponents.js
@@ -3,12 +3,26 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Jennie',
+        address: 'Kim',
+        age: 26
+    };
+
+    handleClick() {
+        console.log("call me", this.state.name);
+    }
+    handleOnMoverOver(event) {
+        console.log(event.pageX);
+    }
+
     render() {
         return (
-            `
-            <div>dsad</div>
-            <div>My first MyComponent</div>
-            `
+            <div>My first MyComponent<button onMouseOver={this.handleOnMoverOver}>Call me</button>
+                {this.state.name} {this.state.address} {this.state.age}
+                <button onClick={this.handleClick}>click me</button>
+
+            </div>
         );
     }
 }
