f80200b0a0feb8ec16ecfe8b832aca74f75a2d1c
Trong Dai <buitrongdai221203@gmail.com>
2023-05-18 18:05:15 +0700

    vd 20

--- src/components/Mycomponents.js
+++ src/components/Mycomponents.js
@@ -9,8 +9,12 @@ class MyComponent extends React.Component {
         age: 26
     };
 
-    handleClick() {
+    handleClick = () => {
         console.log("call me", this.state.name);
+
+        this.setState({
+            name: 'kim jennie'
+        })
     }
     handleOnMoverOver(event) {
         console.log(event.pageX);
@@ -19,8 +23,8 @@ class MyComponent extends React.Component {
     render() {
         return (
             <div>My first MyComponent<button onMouseOver={this.handleOnMoverOver}>Call me</button>
-                {this.state.name} {this.state.address} {this.state.age}
-                <button onClick={this.handleClick}>click me</button>
+                {this.state.name} And {this.state.address} And {this.state.age}
+                <button onClick={(event) => { this.handleClick(event) }}>click me</button>
 
             </div>
         );
