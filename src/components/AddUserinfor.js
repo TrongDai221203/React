f8fb3a2a3e973cebb8d2d669f48006d965e7b24f
Trong Dai <buitrongdai221203@gmail.com>
2023-05-31 13:53:31 +0700

    first commit

--- src/components/AddUserinfor.js
+++ src/components/AddUserinfor.js
@@ -1,48 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 
-class AddUserinfor extends React.Component {
-    state = {
-        name: 'Jennie',
-        address: 'Kim',
+// class AddUserinfor extends React.Component {
+//     state = {
+//         name: 'Jennie',
+//         address: 'Kim',
+//         age: 26
+//     };
+//     handleonChangeInput = (event) => {
+
+//         this.setState({
+//             name: event.target.value
+//         })
+//     }
+//     handleonChangeAge = (event) => {
+//         this.setState({
+//             age: event.target.value
+//         })
+//     }
+//     handlOnSubmit = (event) => {
+//         event.preventDefault();
+//         this.props.handleAddNewUser({
+//             id: Math.floor((Math.random() * 100) + 1),
+//             name: this.state.name,
+//             age: this.state.age
+//         })
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 My first MyComponent
+//                 {this.state.name} And {this.state.address} {this.state.age}
+//                 <form onSubmit={(event) => { this.handlOnSubmit(event) }}>
+//                     <input type="text"
+//                         value={this.state.name}
+//                         onChange={(event) => { this.handleonChangeInput(event) }}
+//                     />
+//                     <input type="text"
+//                         value={this.state.age}
+//                         onChange={(event) => { this.handleonChangeAge(event) }}
+//                     />
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+
+const AddUserinfor = (props) => {
+
+    // const [name, setname] = useState("Jennie");
+    // const [address, setaddress] = useState("Kim");
+    // const [age, setage] = useState("27");
+    const [listUser, setlistUser] = useState({
+        name: 'Jennie Kim',
+        address: 'J E N N I E',
         age: 26
-    };
-    handleonChangeInput = (event) => {
+    })
+    const handleonChangeInput = (event) => {
 
-        this.setState({
+        setlistUser({
             name: event.target.value
         })
     }
-    handleonChangeAge = (event) => {
-        this.setState({
+    const handleonChangeAge = (event) => {
+        setlistUser({
             age: event.target.value
         })
     }
-    handlOnSubmit = (event) => {
+    const handlOnSubmit = (event) => {
         event.preventDefault();
-        this.props.handleAddNewUser({
+        props.handleAddNewUser({
             id: Math.floor((Math.random() * 100) + 1),
-            name: this.state.name,
-            age: this.state.age
+            name: listUser.name,
+            age: listUser.age
         })
     }
-    render() {
-        return (
-            <div>
-                My first MyComponent
-                {this.state.name} And {this.state.address} {this.state.age}
-                <form onSubmit={(event) => { this.handlOnSubmit(event) }}>
-                    <input type="text"
-                        value={this.state.name}
-                        onChange={(event) => { this.handleonChangeInput(event) }}
-                    />
-                    <input type="text"
-                        value={this.state.age}
-                        onChange={(event) => { this.handleonChangeAge(event) }}
-                    />
-                    <button>Submit</button>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div>
+            My first MyComponent
+            {listUser.name} And {listUser.address} {listUser.age}
+            <form onSubmit={(event) => { handlOnSubmit(event) }}>
+                <input type="text"
+                    value={listUser.name}
+                    onChange={(event) => { handleonChangeInput(event) }}
+                />
+                <input type="text"
+                    value={listUser.age}
+                    onChange={(event) => { handleonChangeAge(event) }}
+                />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
 export default AddUserinfor;
--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,71 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
-import logo1 from "./../logo.svg"
 
-class DisplayInfor extends React.Component {
-    constructor(props) {
-        console.log("props");
-        super(props)
-        this.state = {
-            islistusre: false
-        }
+
+//stateless vs setateful
+// class DisplayInfor extends React.Component {
+//     render() {
+
+//         // destructuring array / object
+//         const { listUser } = this.props; //object 
+//         // const listUser = this.props.liseUser
+//         //props => viết tắt properties
+//         // console.table(listUser);
+//         return (
+//             <React.Fragment>
+//                 <div className="display-infor-container">
+//                     <img src={logo1} />
+//                     {true &&
+//                         <>
+//                             {
+//                                 listUser.map((user) => {
+//                                     return (
+//                                         <div>
+//                                             <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                                                 <div>My name is :{user.name}</div>
+//                                                 <div>My age is:{user.age}</div>
+//                                                 <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
+//                                             </div>
+//                                         </div>
+//                                     )
+//                                 })
+//                             }
+//                         </>
+//                     }
+//                 </div>
+//             </React.Fragment>
+//         )
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const { listUser } = props; //object 
+    const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+    const handleShowHideListUser = () => {
+        setShowHideListUser(!isShowHideListUser)
     }
 
-    componentDidMount() {
-        console.log('checkk>>>>>>>');
+
+    useEffect(() => {
+        console.log("useEffect");
         setTimeout(() => {
-            document.title = 'Jennie Kim'
-        }, 3000)
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('checkk>>>>>>>1', this.props, prevProps);
-        if (this.props.listUser.length === 5) {
-            alert('mee')
+            document.title = 'Jennie'
+        }, 2000)
+        if (listUser.length === 5) {
+            alert('5 user')
         }
-    }
-    handleShowHide = () => {
-        this.setState({
-            islistusre: !this.state.islistusre
-        })
-    }
-    render() {
-        console.log('Checkk>>> reder 111111');
+    }, [listUser])
+    return (
 
-        // destructuring array / object
-        const { listUser } = this.props; //object 
-        // const listUser = this.props.liseUser
-        //props => viết tắt properties
-        // console.table(listUser);
-        return (
-            <React.Fragment>
-                <div className="display-infor-container">
-                    <img src={logo1} />
-                    <div>
-                        <span onClick={() => { this.handleShowHide() }}>
-
-                            {this.state.islistusre === true ? "Hide list user:" : "Show list user:"}
-                        </span>
-                    </div>
-                    {this.state.islistusre &&
-                        <>
-                            {
-                                listUser.map((user) => {
-                                    return (
-                                        <div>
-                                            <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                                <div>My name is :{user.name}</div>
-                                                <div>My age is:{user.age}</div>
-                                                <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
-                                            </div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </>
-                    }
+        <>
+            <div className="display-infor-container">
+                <div>
+                    <span onClick={() => handleShowHideListUser()}>
+                        {isShowHideListUser === true ? "Hide list user" : "Show list user"}</span>
                 </div>
-            </React.Fragment>
-        )
-    }
+                {isShowHideListUser &&
+                    <>
+                        {
+                            listUser.map((user, index) => {
+                                return (
+                                    <div>
+                                        <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                            <div>My name is :{user.name}</div>
+                                            <div>My age is:{user.age}</div>
+                                            <button onClick={() => { props.handleDeleteUser(user.id) }}>Delete</button>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </>
+                }
+            </div>
+            <div>haidas</div>
+        </>
+    )
 }
 export default DisplayInfor;
